5bfaf4819da6b605ec91fa138deba8cf98292af1
LuisEfe <luisefediazm@gmail.com>
2019-08-12 10:05:33 -0500

    Load changes

--- BasicCal.py
+++ BasicCal.py
@@ -1,6 +1,10 @@
 # Developer: Hola mijo
 # Basic calculator v1.0
 
+#Calling library
+import os
+#Using library for to clean screen
+os.system ("cls")
 print ("Pruebe esto mijo")
 
 a = 15
@@ -8,4 +12,3 @@ b = 25
 x = a+b
 
 print ("Hi, the add is: ", x)
-git 
