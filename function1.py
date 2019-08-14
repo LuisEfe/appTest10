29c0049e87c26c4a297452b6bf4f1adad8368ce9
LuisEfe <luisefediazm@gmail.com1>
2019-08-14 10:17:03 -0500

    Uso de funciones

---
+++ function1.py
@@ -0,0 +1,15 @@
+#Function1: Multiply two numbers
+
+#Using libraries
+import os
+#Using fucntions
+def calc(a,b): 
+    add = a+b
+    return add
+
+
+#main
+os.system("cls")
+n1 = int (input("Press number 1: "))
+n2 = int (input("Press number 2: "))
+print ("The answer is: ", calc (n1,n2))
