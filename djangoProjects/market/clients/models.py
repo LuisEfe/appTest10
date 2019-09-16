931a98eca72e0c1fbef34a036b7892438e79374a
LuisEfe <luisefediazm@gmail.com>
2019-09-16 10:20:56 -0500

    v6

--- djangoProjects/market/clients/models.py
+++ djangoProjects/market/clients/models.py
@@ -10,3 +10,11 @@ class Choice (models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+class Users (models.Model):    
+    user_name = models.CharField(max_length=500)
+    email = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=20)
+    last_name = models.CharField(max_length=20)
+    age = models.IntegerField(default=0)
+    
--- djangoProjects/market/market/settings.py
+++ djangoProjects/market/market/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'products.apps.ProductsConfig',
     'clients.apps.ClientsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -74,13 +75,24 @@ WSGI_APPLICATION = 'market.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'market',
+        'USER': 'postgres',
+        'PASSWORD': 'unicesmag',
+        'HOST': 'localhost', #Or localhost
+        'PORT': '5432', #Or empty for default: 5432
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
